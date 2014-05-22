68ce45181e7a31b71c1c2d568eb7fca2c95eb072
Igor Santos <igorsantos07@gmail.com>
2014-05-22 20:35:42 -0300

    Adding portuguese translation

---
+++ messages/pt/edt.php
@@ -0,0 +1,37 @@
+<?php
+return array(
+	"(filtered from _MAX_ total entries)" => "(filtrado de <strong>_MAX_</strong> registros no total)",
+	": activate to sort column ascending" => ": ative para ordem ascendente",
+	": activate to sort column descending" => ": ative para ordem decrescente",
+	/*
+	"First" => "Primeira",
+	"Last" => "Última",
+	"Next" => "Próxima",
+	"Previous" => "Anterior",
+	 */
+	"First" => "&laquo;",
+	"Last" => "&raquo;",
+	"Next" => "&rsaquo;",
+	"Previous" => "&lsaquo;",
+
+	"Loading..." => "Carregando...",
+	"No data available in table" => "Sem dados dispon&iacute;veis na tabela",
+	"No matching records found" => "Nenhum registro foi encontrado",
+	"Processing..." => "Processando...",
+	"Search:" => "Busca:",
+	"Show _MENU_ entries" => Exibir _MENU_ registros",
+	"Showing 0 to 0 of 0 entries" => "Exibindo 0 registro",
+	"Showing _START_ to _END_ of _TOTAL_ entries" => "Exibindo <strong>_START_</strong> at&acute; <strong>_END_</strong> de <strong>_TOTAL_</strong> registros",
+	"Only related" => "Somente relacionados",
+	"History" => "Histórico",
+	"Refresh" => "Atualizar",
+	"Configure" => "Configurar",
+	"Print" => "Imprimir",
+	"Save as CSV" => "Zapisz jako CSV",
+	"Add new" => "Adicionar novo",
+	'Select all' => 'Selecionar todos',
+	'Deselect all' => 'Desmarcar todos',
+	'Select on page' => 'Selecionar na página',
+	'Deselect on page' => 'Desmarcar na página',
+	'Options fnServerParams and fnServerData are reserved and cannot be set. Use the serverParams and serverData properties instead.' => 'As opções fnServerParams e fnServerData são reservadas e não podem ser sobrescritas. Use serverParams e serverData ao invés.',
+);
